53af93de5d1d170312ede8205903f5d0574a787f
Sachin Sheelavant <sheelavantsachin@gmail.com>
2021-01-19 20:18:06 +0530

    updated vendor controller

--- src/main/java/com/app/controller/VendorController.java
+++ src/main/java/com/app/controller/VendorController.java
@@ -1,25 +1,16 @@
 package com.app.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.cust_excs.ResourceNotFoundException;
-import com.app.entities.AddressModel;
 import com.app.entities.HotelModel;
 import com.app.entities.UserModel;
-import com.app.repository.AddressRepository;
 import com.app.repository.HotelRepository;
 import com.app.repository.MenuItemRepository;
 import com.app.repository.UserRepository;
@@ -34,9 +25,6 @@ public class VendorController {
 
 	@Autowired
 	private HotelRepository hotelRepo;
-
-	@Autowired
-	private AddressRepository addressRepo;
 	
 	@Autowired
 	private MenuItemRepository menuItemRepository;
@@ -55,45 +43,6 @@ public class VendorController {
 		HotelModel vendorHotel = getHotelByVendorEmail(vendorEmail);
 		menuItemRepository.findByHotelId(vendorHotel.getId(), pageable);
 		return ResponseEntity.ok(menuItemRepository.findByHotelId(vendorHotel.getId(), pageable));
-	}
-
-	@PutMapping("/hotel")
-	public ResponseEntity<?> updateHotelDetails(@Valid @RequestBody HotelModel hotelRequest,
-			@CurrentSecurityContext(expression = "authentication.name") String vendorEmail) {
-
-		HotelModel vendorHotel = getHotelByVendorEmail(vendorEmail);
-
-		vendorHotel.setHotelName(hotelRequest.getHotelName());
-		vendorHotel.setMobileNo(hotelRequest.getMobileNo());
-		AddressModel reqAdd = hotelRequest.getAddress();
-
-		if (addressRepo.existsById(reqAdd.getId())) {
-			addressRepo.findById(reqAdd.getId()).map(hotelAdd -> {
-				hotelAdd.setAddressLine1(reqAdd.getAddressLine1());
-				hotelAdd.setAddressLine2(reqAdd.getAddressLine2());
-				hotelAdd.setPincode(reqAdd.getPincode());
-				hotelAdd.setCity(reqAdd.getCity());
-				hotelAdd.setState(reqAdd.getState());
-				hotelAdd.setCountry(reqAdd.getCountry());
-				return addressRepo.save(hotelAdd);
-			});
-		} else {
-			vendorHotel.setAddress(reqAdd);
-		}
-
-		hotelRepo.saveAndFlush(vendorHotel);
-
-		return ResponseEntity.ok(vendorHotel);
-	}
-	
-	@DeleteMapping("/hotel/menu/{menuId}")
-	public ResponseEntity<?> deleteComment(
-			@PathVariable(value = "menuId") Long menuId) {
-		return menuItemRepository.findById(menuId).map(menu -> {
-			menuItemRepository.delete(menu);
-			return ResponseEntity.ok().build();
-		}).orElseThrow(() -> new ResourceNotFoundException(
-				"Menu Item not found with id " + menuId));
 	}
 	
 	private HotelModel getHotelByVendorEmail(String vendorEmail) {
