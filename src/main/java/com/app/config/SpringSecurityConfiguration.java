f5553cbd82e43e0474b4acf5b2549a299364c70c
Sachin Sheelavant <sheelavantsachin@gmail.com>
2022-06-20 10:08:41 +0530

    added spring actuator

--- src/main/java/com/app/config/SpringSecurityConfiguration.java
+++ src/main/java/com/app/config/SpringSecurityConfiguration.java
@@ -8,13 +8,14 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +45,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
 	}
+	
 
 	@Override
 	public void configure(WebSecurity web) throws Exception {
@@ -54,6 +56,7 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().authorizeRequests().antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+				.antMatchers("/actuator/**").permitAll()
 				.antMatchers("/api/login", "/api/register", "/cart/**", "/api/account/**", "/order/**").permitAll()
 				.antMatchers(HttpMethod.GET, "/hotels", "/hotels/**").permitAll()
 				.antMatchers(HttpMethod.POST, "/hotels", "/hotels/**").hasAnyRole("ADMIN", "VENDOR")
